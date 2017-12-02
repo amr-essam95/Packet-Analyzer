65564306f77fdb133d4eb6feb764ac1d090943c8
Hatem <Mohamed.hatem1911@gmail.com>
2017-12-02 22:53:39 +0200

    designing the skeleton of the front end

--- window.py
+++ window.py
@@ -16,8 +16,8 @@ class MyWindow(QtGui.QMainWindow,Ui_MainWindow):    # any super class is okay
         self.setWindowState(QtCore.Qt.WindowMaximized)
         self.startCaptureBtn.triggered.connect(lambda:self.startCaptureBtnClicked(self.startCaptureBtn))
         self.stopCaptureBtn.triggered.connect(lambda:self.stopCaptureBtnClicked(self.stopCaptureBtn))
-
-
+        self.packetList=[]
+        self.filter=""
 ######################################################
     def startCaptureBtnClicked(self,btn):
       
@@ -26,19 +26,15 @@ class MyWindow(QtGui.QMainWindow,Ui_MainWindow):    # any super class is okay
             self.stackedWidget.setCurrentIndex(1)
             self.startCaptureBtn.setEnabled(False)
             self.stopCaptureBtn.setEnabled(True)
-            
         else:
-            
             self.startCaptureBtn.setEnabled(False)
             self.stopCaptureBtn.setEnabled(True)
             self.startCapture()
 #######################################################
-    def stopCaptureBtnClicked(self,btn):
-      
+    def stopCaptureBtnClicked(self,btn):      
         self.stopCapture()
         self.startCaptureBtn.setEnabled(True)
-        self.stopCaptureBtn.setEnabled(False)
-    
+        self.stopCaptureBtn.setEnabled(False)   
 #########################################################
     def startCapture(self):
         pass
@@ -48,43 +44,74 @@ class MyWindow(QtGui.QMainWindow,Ui_MainWindow):    # any super class is okay
 #########################################################
     def addEntry(self):
         pass
+#########################################################
+    def addPacket(self,packet):
+        self.packetList.append(packet)
+#########################################################
+    def addPacketToTable(self,packet):
+        pass
+#########################################################
+    def filterPacket(self,packet):
+        pass
+#########################################################
+    def applyNewFilter(self):
+        pass
+#########################################################
+    def showPacketDescription(self,packet):
+        pass
+#########################################################
+    def showPacketHexadecimal(self,packet):
+        pass
+#########################################################
+    def applyFilterClicked(self,btn):
+        pass
+#########################################################
+    def saveSession(self):
+        pass
+#########################################################
+    def saveBtnClicked(self,btn):
+        pass
+#########################################################
+    def exitBtnClicked(self,btn):
+        pass
+#########################################################
+    def exitProgram(self):
+        pass
 app = QtGui.QApplication(sys.argv)
 window = MyWindow()
 window.show()
 app.exec_()
 ##########################################################
-# import threading
-# import time
-# class ThreadingClass(QtCore.QThread):
+import threading
+import time
+class ThreadingClass(QtCore.QThread):
 
-#   """ Threading example class
-#   The run() method will be started and it will run in the background
-#   until the application exits.
-#   """
-#   def __init__(self, interval=1):
-#     """ Constructor
-#     :type interval: int
-#     :param interval: Check interval, in seconds
-#     """
-#     QtCore.QThread.__init__(self, parent=None)
-#     #threading.Thread.__init__(self, *args, **kwargs)
-#     self.interval = interval
-#     thread = threading.Thread(target=self.run, args=())
-#     self.signal = QtCore.SIGNAL("signal")
-#     thread.daemon = True # Daemonize thread
-#     thread.start() # Start the execution
-#   def run(self):
-#     """ Method that runs forever """
-#     while True:
-#         
-#         time.sleep(self.interval)
+  """ Threading example class
+  The run() method will be started and it will run in the background
+  until the application exits.
+  """
+  def __init__(self, interval=1):
+    """ Constructor
+    :type interval: int
+    :param interval: Check interval, in seconds
+    """
+    QtCore.QThread.__init__(self, parent=None)
+    #threading.Thread.__init__(self, *args, **kwargs)
+    self.interval = interval
+    thread = threading.Thread(target=self.run, args=())
+    self.signal = QtCore.SIGNAL("signal")
+    thread.daemon = True # Daemonize thread
+    thread.start() # Start the execution
+  def run(self):
+    """ Method that runs forever """
+        while True:
+            time.sleep(self.interval)
+            packet= self.getPacket()
+            window.addPacket(packet)
+    def getPacket()
+        pass
 
-# thread = ThreadingClass()
-
-
-
-
-
-# #############################################################
+thread = ThreadingClass()
+##############################################################
 
 
