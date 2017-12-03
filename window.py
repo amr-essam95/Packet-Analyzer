a99647794eafc6fd788569197cfb5a5f636506c7
mossama95@hotmail.com <mossama95@hotmail.com>
2017-12-03 20:37:11 +0200

    adding a function stop to thread and start and stop function to their buttons in GUI

--- window.py
+++ window.py
@@ -2,7 +2,44 @@ import PyQt4
 from PyQt4 import QtGui, QtCore,uic
 import socket
 import sys
+import threading
+import time
+####################################################################
+class ThreadingClass(QtCore.QThread):
 
+  """ Threading example class
+  The run() method will be started and it will run in the background
+  until the application exits.
+  """
+  def __init__(self, interval=1):
+    """ Constructor
+    :type interval: int
+    :param interval: Check interval, in seconds
+    """
+    QtCore.QThread.__init__(self, parent=None)
+    #threading.Thread.__init__(self, *args, **kwargs)
+    self.interval = interval
+    thread = threading.Thread(target=self.run, args=())
+    self.signal = QtCore.SIGNAL("signal")
+    thread.daemon = True # Daemonize thread
+    #thread.start() # Start the execution
+    #print("thread started")
+    self._stop_event = threading.Event()
+  def run(self):
+    """ Method that runs forever """
+
+    while True:
+        #print("thread running")  #to test run function
+        time.sleep(self.interval)
+        #packet= self.getPacket()
+        #window.addPacket(packet)
+    #def getPacket():
+        pass
+  def stop(self):
+    window.thread.terminate()
+    #print("thread stopped")  #to test stop function
+
+#################################################################
 
 qtCreatorFile="NetworkAnalyzer.ui"
 Ui_MainWindow,QtBaseClass = uic.loadUiType(qtCreatorFile) 
@@ -18,6 +55,8 @@ class MyWindow(QtGui.QMainWindow,Ui_MainWindow):    # any super class is okay
         self.stopCaptureBtn.triggered.connect(lambda:self.stopCaptureBtnClicked(self.stopCaptureBtn))
         self.packetList=[]
         self.filter=""
+        self.thread = ThreadingClass()
+
 ######################################################
     def startCaptureBtnClicked(self,btn):
       
@@ -37,9 +76,11 @@ class MyWindow(QtGui.QMainWindow,Ui_MainWindow):    # any super class is okay
         self.stopCaptureBtn.setEnabled(False)   
 #########################################################
     def startCapture(self):
+        self.thread.start()
         pass
 #########################################################
     def stopCapture(self):
+        self.thread.stop()
         pass
 #########################################################
     def addEntry(self):
@@ -82,36 +123,9 @@ window = MyWindow()
 window.show()
 app.exec_()
 ##########################################################
-import threading
-import time
-class ThreadingClass(QtCore.QThread):
 
-  """ Threading example class
-  The run() method will be started and it will run in the background
-  until the application exits.
-  """
-  def __init__(self, interval=1):
-    """ Constructor
-    :type interval: int
-    :param interval: Check interval, in seconds
-    """
-    QtCore.QThread.__init__(self, parent=None)
-    #threading.Thread.__init__(self, *args, **kwargs)
-    self.interval = interval
-    thread = threading.Thread(target=self.run, args=())
-    self.signal = QtCore.SIGNAL("signal")
-    thread.daemon = True # Daemonize thread
-    thread.start() # Start the execution
-  def run(self):
-    """ Method that runs forever """
-        while True:
-            time.sleep(self.interval)
-            packet= self.getPacket()
-            window.addPacket(packet)
-    def getPacket()
-        pass
-
-thread = ThreadingClass()
+#thread = ThreadingClass()
 ##############################################################
+
 
 
