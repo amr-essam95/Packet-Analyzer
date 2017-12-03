13727782f89788227e54eff9f0d37c3ff246bc09
Hatem <Mohamed.hatem1911@gmail.com>
2017-12-04 00:40:29 +0200

    Implementing Front end methods

--- window.py
+++ window.py
@@ -6,62 +6,57 @@ import threading
 import time
 ####################################################################
 class ThreadingClass(QtCore.QThread):
-
-  """ Threading example class
-  The run() method will be started and it will run in the background
-  until the application exits.
-  """
-  def __init__(self, interval=1):
-    """ Constructor
-    :type interval: int
-    :param interval: Check interval, in seconds
-    """
-    QtCore.QThread.__init__(self, parent=None)
-    #threading.Thread.__init__(self, *args, **kwargs)
-    self.interval = interval
-    thread = threading.Thread(target=self.run, args=())
-    self.signal = QtCore.SIGNAL("signal")
-    thread.daemon = True # Daemonize thread
-    #thread.start() # Start the execution
-    #print("thread started")
-    self._stop_event = threading.Event()
-  def run(self):
-    """ Method that runs forever """
-
-    while True:
-        #print("thread running")  #to test run function
-        time.sleep(self.interval)
-        #packet= self.getPacket()
-        #window.addPacket(packet)
-    #def getPacket():
-        pass
-  def stop(self):
-    window.thread.terminate()
-    #print("thread stopped")  #to test stop function
-
+	""" Threading example class
+	The run() method will be started and it will run in the background
+	until the method stop is called.
+	"""
+	def __init__(self, interval=1):
+		""" Constructor
+		:type interval: int
+		:param interval: Check interval, in seconds
+		"""
+		QtCore.QThread.__init__(self, parent=None)
+		#threading.Thread.__init__(self, *args, **kwargs)
+		self.interval = interval
+		thread = threading.Thread(target=self.run, args=())
+		self.signal = QtCore.SIGNAL("signal")
+		thread.daemon = True # Daemonize thread
+		#thread.start() # Start the execution
+		#print("thread started")
+		self._stop_event = threading.Event()
+  	def run(self):
+		""" Method that runs forever """
+		while True:
+			#print("thread running")  #to test run function
+			time.sleep(self.interval)
+			packet= self.getPacket()
+			window.addPacket(packet)
+	def getPacket(self):
+		pass
+  	def stop(self):
+		window.thread.terminate()
+		#print("thread stopped")  #to test stop function
 #################################################################
-
 qtCreatorFile="NetworkAnalyzer.ui"
 Ui_MainWindow,QtBaseClass = uic.loadUiType(qtCreatorFile) 
-
-###############################
+#######################################################
 class MyWindow(QtGui.QMainWindow,Ui_MainWindow):    # any super class is okay
-
-    def __init__(self, parent=None):
-        QtGui.QMainWindow.__init__(self)
-        Ui_MainWindow.__init__(self)
-        self.setupUi(self)
-        self.setWindowState(QtCore.Qt.WindowMaximized)
-        self.startCaptureBtn.triggered.connect(lambda:self.startCaptureBtnClicked(self.startCaptureBtn))
-        self.stopCaptureBtn.triggered.connect(lambda:self.stopCaptureBtnClicked(self.stopCaptureBtn))
-        self.packetList=[]
-        self.filter=""
-        self.thread = ThreadingClass()
-
+	def __init__(self, parent=None):
+		QtGui.QMainWindow.__init__(self)
+		Ui_MainWindow.__init__(self)
+		self.setupUi(self)
+		self.setWindowState(QtCore.Qt.WindowMaximized)
+		self.startCaptureBtn.triggered.connect(lambda:self.startCaptureBtnClicked(self.startCaptureBtn))
+		self.stopCaptureBtn.triggered.connect(lambda:self.stopCaptureBtnClicked(self.stopCaptureBtn))
+		self.actionExitBtn.triggered.connect(lambda:self.exitBtnClicked(self.actionExitBtn))
+		self.packetList=[]
+		self.filter=""
+		self.tableSize=0
+		self.thread = ThreadingClass()
 ######################################################
 	def startCaptureBtnClicked(self,btn):
-	  
 		if(self.stackedWidget.currentIndex()==0):
+			#assure that connextion is selected and send it to thread
 			self.startCapture()
 			self.stackedWidget.setCurrentIndex(1)
 			self.startCaptureBtn.setEnabled(False)
@@ -76,37 +71,58 @@ class MyWindow(QtGui.QMainWindow,Ui_MainWindow):    # any super class is okay
 		self.startCaptureBtn.setEnabled(True)
 		self.stopCaptureBtn.setEnabled(False)   
 #########################################################
-    def startCapture(self):
-        self.thread.start()
-        pass
+	def startCapture(self):
+		self.thread.start()
 #########################################################
-    def stopCapture(self):
-        self.thread.stop()
-        pass
-#########################################################
-	def addEntry(self):
-		pass
+	def stopCapture(self):
+		self.thread.stop()
 #########################################################
 	def addPacket(self,packet):
 		self.packetList.append(packet)
+		filterOutput=self.passFilter(packet)
+		if(filterOutput==True):
+			self.addPacketToTable(packet)
 #########################################################
 	def addPacketToTable(self,packet):
-		pass
+		self.tableSize += 1
+		self.table.insertRow(self.size)
+		self.table.setItem(self.size,0, QtGui.QTableWidgetItem(str(packet['No.'])))
+		self.table.setItem(self.size,1, QtGui.QTableWidgetItem(str(packet['Time'])))
+		self.table.setItem(self.size,2, QtGui.QTableWidgetItem(str(packet['Source'])))
+		self.table.setItem(self.size,3, QtGui.QTableWidgetItem(str(packet['Destination'])))
+		self.table.setItem(self.size,4, QtGui.QTableWidgetItem(str(packet['Protocol'])))
+		self.table.setItem(self.size,5, QtGui.QTableWidgetItem(str(packet['Length'])))
+		self.table.setItem(self.size,6, QtGui.QTableWidgetItem(str(packet['Info'])))
 #########################################################
-	def filterPacket(self,packet):
-		pass
+	def passFilter(self,packet):
+		if (self.filter==""):#if there is no filter
+			return True
+		elif(str(packet['Protocol']).lower()==str(self.filter).lower()):
+			return True
+		else :
+			return False
 #########################################################
 	def applyNewFilter(self):
-		pass
+		self.tableSize=0
+		self.table.setRowCount(0)
+		for packet in self.packetList:
+			filterOutput=self.passFilter(packet)
+			if (filterOutput==True):
+				addPacketToTable(packet)
 #########################################################
 	def showPacketDescription(self,packet):
-		pass
+		self.treeWidget.clear()
+		for key, value in packet['Description'].iteritems():
+			#fill the tree
 #########################################################
 	def showPacketHexadecimal(self,packet):
-		pass
+		self.plainTextEdit.setText(packet['Hexa'])
 #########################################################
 	def applyFilterClicked(self,btn):
-		pass
+		newFilter=self.filterLineEdit.text
+		if (newFilter != self.filter):
+			self.filter=newFilter
+			self.applyNewFilter
 #########################################################
 	def saveSession(self):
 		pass
@@ -115,19 +131,26 @@ class MyWindow(QtGui.QMainWindow,Ui_MainWindow):    # any super class is okay
 		pass
 #########################################################
 	def exitBtnClicked(self,btn):
-		pass
+		msg = QtGui.QMessageBox()
+		msg.setIcon(QtGui.QMessageBox.Information)
+		msg.setText("Are you sure you want to exit?")
+		#msg.setInformativeText("By pressing ok the program will be closed")
+		msg.setWindowTitle("Exit Window")
+		msg.setDetailedText("Are you sure dude?")
+		msg.setStandardButtons(QtGui.QMessageBox.Ok | QtGui.QMessageBox.Cancel)
+		msg.buttonClicked.connect(self.exitProgram)
+		retval = msg.exec_()
+		if retval==QtGui.QMessageBox.Ok:
+			self.exitProgram
 #########################################################
-	def exitProgram(self):
-		pass
+	def exitProgram(self,buttonPressed):
+			sys.exit()
+#########################################################
 app = QtGui.QApplication(sys.argv)
 window = MyWindow()
 window.show()
 app.exec_()
 ##########################################################
-
-
-#thread = ThreadingClass()
-##############################################################
 
 
 
